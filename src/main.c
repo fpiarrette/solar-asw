6a766e29742f7d37e3edfe5365884a702402c42d
Fernando Piarrette Caride <fm.piarrette@thalesaleniaspace.com>
2026-08-17 16:23:24 +0200

    Socket config

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@ tmtc_ctx_spi_t tmtc_ctx_spi;
 int main(void) {
 
     /* Initialization */
+    tmtc_ctx_socket.port = 2000;
     tmtc_init_socket(&tmtc_ctx_socket, 0);
     tmtc_init_spi(&tmtc_ctx_spi, 0);
 
