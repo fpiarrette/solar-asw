0ccbb1e7ecd47981ccafa2b4f53433208eec2752
Fernando Piarrette Caride <fm.piarrette@thalesaleniaspace.com>
2026-08-17 17:14:15 +0200

    SIG STOP and INT managed

--- src/main.c
+++ src/main.c
@@ -1,10 +1,31 @@
+#include "utils.h"
 #include "tmtc_socket.h"
 #include "tmtc_spi.h"
 
 #include <signal.h>
+#include <stdlib.h>
 
 tmtc_ctx_socket_t tmtc_ctx_socket;
 tmtc_ctx_spi_t tmtc_ctx_spi;
+
+int stopped;
+int terminated;
+
+static void signal_handler(int signal) {
+    switch (signal) {
+        case SIGINT:
+        terminated = 1;
+        break;
+        case SIGTSTP:
+        stopped = 1;
+        break;
+        case SIGCONT:
+        stopped = 0;
+        break;
+        default:
+        break;
+    }
+}
 
 int main(void) {
 
@@ -13,8 +34,26 @@ int main(void) {
     tmtc_init_socket(&tmtc_ctx_socket, 0);
     tmtc_init_spi(&tmtc_ctx_spi, 0);
 
-    /* Main loop */
+    struct sigaction psa;
+    psa.sa_handler = signal_handler;
+    sigaction(SIGINT, &psa, NULL);
+    sigaction(SIGTSTP, &psa, NULL);
+    sigaction(SIGCONT, &psa, NULL);
 
+    /* Main loop */
+    while (!terminated) {
+        if (!stopped) {
+            /* do the job */
+        }
+        else {
+            /* sleep for 100mS */
+            utils_sleep(100);
+        }
+    }
+
+    /* stop/deallocate componets */
+    tmtc_stop_socket(&tmtc_ctx_socket, 0);
+    tmtc_stop_spi(&tmtc_ctx_spi, 0);
 
     return 0;
 }
