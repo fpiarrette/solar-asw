1d7d3bad07d4ff5a1e642bc0b9ba5e537ad08497
Fernando Piarrette Caride <fm.piarrette@thalesaleniaspace.com>
2026-08-20 10:20:16 +0200

    Partial implementation of client and server socket channels

--- src/ChannelSocketClient.cpp
+++ src/ChannelSocketClient.cpp
@@ -1,15 +1,27 @@
 #include "ChannelSocketClient.h"
 
+#include <sys/socket.h>
+#include <netinet/in.h>
+
 void ChannelSocketClient::init(void)
 {
+    /* socket is created */
+    fd = socket(AF_INET, SOCK_STREAM, 0);
 }
 
 void ChannelSocketClient::start(void)
 {
+    /* server address definition */
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    connect(fd, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
 }
 
 void ChannelSocketClient::send(char *data, int size)
 {
+    /* send(fd, data, size, 0); */
 }
 
 void ChannelSocketClient::receive(char *data, int size, int *received)
--- src/ChannelSocketServer.cpp
+++ src/ChannelSocketServer.cpp
@@ -1,11 +1,23 @@
 #include "ChannelSocketServer.h"
 
+#include <sys/socket.h>
+#include <netinet/in.h>
+
 void ChannelSocketServer::init(void)
 {
+    /* protocol AF_NET -> IPv4, SOCK_STREAM -> TCP socket */
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+    /* bind to the address */
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    bind(fd, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
 }
 
 void ChannelSocketServer::start(void)
 {
+    listen(fd, 5);
 }
 
 void ChannelSocketServer::send(char *data, int size)
@@ -14,8 +26,12 @@ void ChannelSocketServer::send(char *data, int size)
 
 void ChannelSocketServer::receive(char *data, int size, int *received)
 {
+    int clientSocket = accept(fd, nullptr, nullptr);
+    /* FIXME is blocking */
+    recv(fd, data, size, 0);
 }
 
 void ChannelSocketServer::stop(void)
 {
+    /* close(fd); */
 }
