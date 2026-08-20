89b1f1be2f3830ca7f1200582f2979ed03871545
Fernando Piarrette Caride <fm.piarrette@thalesaleniaspace.com>
2026-08-20 11:58:26 +0200

    Improved client channel implementation

--- src/ChannelSocketClient.cpp
+++ src/ChannelSocketClient.cpp
@@ -1,11 +1,13 @@
 #include "ChannelSocketClient.h"
 
-#include <sys/socket.h>
+#include <fcntl.h>
 #include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 void ChannelSocketClient::init(void)
 {
-    /* socket is created */
+    /* client socket is created */
     fd = socket(AF_INET, SOCK_STREAM, 0);
 }
 
@@ -17,17 +19,25 @@ void ChannelSocketClient::start(void)
     serverAddress.sin_port = htons(8080);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
     connect(fd, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+    /* change client socket to NON BLOCKING mode */
+    if (setNonBlock(fd) != CHANNEL_E_OK) {
+        /* FIXME manage error */
+    }
 }
 
 void ChannelSocketClient::tx(char *data, int size)
 {
-    /* send(fd, data, size, 0); */
+    send(fd, data, size, 0);
 }
 
 void ChannelSocketClient::rx(char *data, int size, int *received)
 {
+    /* FIXME manage erros */
+    secureRx(fd, data, size, received);
 }
 
 void ChannelSocketClient::stop(void)
 {
+    shutdown(fd, SHUT_RDWR);
+    close(fd);
 }
