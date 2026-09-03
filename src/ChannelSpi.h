06cfe44032619443e37f323ac4e23d3420a99ce0
Fernando Piarrette Caride <fm.piarrette@thalesaleniaspace.com>
2026-09-03 17:36:05 +0200

    Auto call to stop

--- src/ChannelSpi.h
+++ src/ChannelSpi.h
@@ -6,6 +6,11 @@
 class ChannelSpi : public Channel
 {
 public:
+    ~ChannelSpi()
+    {
+        /* make sure that stop function is called */
+        stop();
+    }
     Channel::Error init(void);
     Channel::Error start(void);
     Channel::Error tx(char *data, int size, int *transmitted);
