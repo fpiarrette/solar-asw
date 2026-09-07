ae1fe2162b7e1f8c7584655d4b83e817e992b769
Fernando Piarrette <fernando.piarrette@gmail.com>
2026-09-07 21:10:50 +0200

    Cosmetic

--- tools/net/validate.sh
+++ tools/net/validate.sh
@@ -6,18 +6,18 @@ project_dir=$script_dir/../..
 
 if [ ! -n "$SOLAR_TARGET_IP_ADDRESS" ]
 then
-  echo "You have to prepare.sh first!"
+  echo "You have to prepare network environment first!"
   exit -1
 fi
 
 if [ ! -n "$SOLAR_TARGET_KILL_PORT" ]
 then
-  echo "You have to prepare.sh first!"
+  echo "You have to prepare network environment first!"
   exit -1
 fi
 
 if [ ! -n "$SOLAR_TARGET_REST_PORT" ]
 then
-  echo "You have to prepare.sh first!"
+  echo "You have to prepare network environment first!"
   exit -1
 fi
