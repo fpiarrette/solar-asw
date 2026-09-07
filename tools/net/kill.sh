4ffeb841bef7ad95a86ba8ef0c430fb17d52590a
Fernando Piarrette <fernando.piarrette@gmail.com>
2026-09-07 20:07:14 +0200

    Little fix

--- tools/net/kill.sh
+++ tools/net/kill.sh
@@ -4,7 +4,7 @@
 script_dir=$(readlink -f $(pwd)/$(dirname "$0"))
 project_dir=$script_dir/../..
 
-. $project_dir/tools/rest/validate.sh
+. $project_dir/tools/net/validate.sh
 
 echo "sending kill $SOLAR_TARGET_IP_ADDRESS:$SOLAR_TARGET_KILL_PORT"
 
