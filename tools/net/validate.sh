2b0f85c7aa2ff5def063660f8fe3fa48b2fbb747
Fernando Piarrette Caride <fm.piarrette@thalesaleniaspace.com>
2026-09-07 21:39:21 +0200

    Littler error value fix

--- tools/net/validate.sh
+++ tools/net/validate.sh
@@ -7,17 +7,17 @@ project_dir=$script_dir/../..
 if [ ! -n "$SOLAR_TARGET_IP_ADDRESS" ]
 then
   echo "You have to prepare network environment first!"
-  exit -1
+  exit 1
 fi
 
 if [ ! -n "$SOLAR_TARGET_KILL_PORT" ]
 then
   echo "You have to prepare network environment first!"
-  exit -1
+  exit 1
 fi
 
 if [ ! -n "$SOLAR_TARGET_REST_PORT" ]
 then
   echo "You have to prepare network environment first!"
-  exit -1
+  exit 1
 fi
