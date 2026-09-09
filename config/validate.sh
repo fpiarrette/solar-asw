0dce306edf7eb035e1852516647db1ef719ec08a
Fernando Piarrette <fernando.piarrette@gmail.com>
2026-09-09 13:43:08 +0200

    Common function

--- config/validate.sh
+++ config/validate.sh
@@ -1,61 +1,57 @@
 #!/bin/sh
 
+show_error()
+{
+  echo "Environment shall be prepared first!"
+  exit 1
+}
+
 if [ ! -n "$PLATFORM" ]
 then
-  echo "You have to prepare environment first!"
-  exit 1
+  show_error
 fi
 
 if [ ! -n "$SOLAR_TARGET_IP_ADDRESS" ]
 then
-  echo "You have to prepare environment first!"
-  exit 1
+  show_error
 fi
 
 if [ ! -n "$SOLAR_TARGET_KILL_PORT" ]
 then
-  echo "You have to prepare environment first!"
-  exit 1
+  show_error
 fi
 
 if [ ! -n "$SOLAR_TARGET_REST_PORT" ]
 then
-  echo "You have to prepare environment first!"
-  exit 1
+  show_error
 fi
 
 if [ ! -n "$DOCKER_IMAGE_NAME" ]
 then
-  echo "You have to prepare environment first!"
-  exit 1
+  show_error
 fi
 
 if [ ! -n "$BINARY_NAME" ]
 then
-  echo "You have to prepare environment first!"
-  exit 1
+  show_error
 fi
 
 if [ ! -n "$OUTPUT_DIR" ]
 then
-  echo "You have to prepare environment first!"
-  exit 1
+  show_error
 fi
 
 if [ ! -n "$PLATFORM_ID" ]
 then
-  echo "You have to prepare environment first!"
-  exit 1
+  show_error
 fi
 
 if [ ! -n "$PLATFORM_HOST" ]
 then
-  echo "You have to prepare environment first!"
-  exit 1
+  show_error
 fi
 
 if [ ! -n "$PLATFORM_TARGET" ]
 then
-  echo "You have to prepare environment first!"
-  exit 1
+  show_error
 fi
