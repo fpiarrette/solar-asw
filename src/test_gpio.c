86cedd6194d25ef0d5e03973d6128c273538dad0
Fernando Piarrette <fernando.piarrette@gmail.com>
2026-09-09 13:44:07 +0200

    GPIO test improved, two cases, read and write, argument check, proper execution flow and error check

--- src/test_gpio.c
+++ src/test_gpio.c
@@ -2,6 +2,75 @@
 
 #include "Gpio.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
+void getLineValue(char *deviceName, char *line)
+{
+    int l, v;
+    GpioAbstract::Error result;
+
+    if (sscanf(line, "%d", &l) != 1)
+    {
+        L_ERROR("%s can not be converted to int", line);
+        return;
+    }
+
+    result = Gpio::getInstance()->configure(l, GpioAbstract::Type::IN);
+
+    if (result != GpioAbstract::Error::E_OK)
+    {
+        L_ERROR("line %d can not be configured as input", l);
+        return;
+    }
+
+    result = Gpio::getInstance()->get(l, &v);
+
+    if (result != GpioAbstract::Error::E_OK)
+    {
+        L_ERROR("error reading line %d", l);
+        return;
+    }
+
+    L_NOTICE("line %d is %s", l, v == 0 ? "off" : "on");
+}
+
+void setLineValue(char *deviceName, char *line, char *value)
+{
+    int l, v;
+    GpioAbstract::Error result;
+
+    if (sscanf(line, "%d", &l) != 1)
+    {
+        L_ERROR("%s can not be converted to int", line);
+        return;
+    }
+
+    if (sscanf(value, "%d", &v) != 1)
+    {
+        L_ERROR("%s can not be converted to int", value);
+        return;
+    }
+
+    result = Gpio::getInstance()->configure(l, GpioAbstract::Type::OUT);
+
+    if (result != GpioAbstract::Error::E_OK)
+    {
+        L_ERROR("line %d can not be configured as output", l);
+        return;
+    }
+
+    result = Gpio::getInstance()->set(l, v);
+
+    if (result != GpioAbstract::Error::E_OK)
+    {
+        L_ERROR("error writing line %d", l);
+        return;
+    }
+
+    L_NOTICE("line %d set to %d", l, v);
+}
+
 int main(int argc, char *argv[])
 {
     Logger::config(Logger::Type::STDOUT);
@@ -11,18 +80,25 @@ int main(int argc, char *argv[])
 
     L_INFO("Starting...");
 
-    int line, value;
-    GpioAbstract::Error result;
-
     Gpio::configure(Gpio::Type::MODULE);
+    Gpio::getInstance()->setDeviceName(argv[1]);
 
-    line = 1;
+    switch (argc)
+    {
+    case 3:
+        getLineValue(argv[1], argv[2]);
+        break;
+    case 4:
+        setLineValue(argv[1], argv[2], argv[3]);
+        break;
 
-    Gpio::getInstance()->configure(line, GpioAbstract::Type::IN);
+    default:
+        L_ERROR("Wrong number of arguments");
+        L_NOTICE("2 arguments: <device> <line>");
+        L_NOTICE("3 arguments: <device> <line> <value>");
 
-    result = Gpio::getInstance()->get(line, &value);
-
-    L_DEBUG("line %d value %d with result %d", line, value, result);
+        break;
+    }
 
     Gpio::getInstance()->stop();
 
