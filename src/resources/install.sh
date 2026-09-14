37ee4353fd92a13777c372bc04224a7dd988ca1a
Fernando Piarrette <fernando.piarrette@gmail.com>
2026-09-14 16:09:28 +0200

    Specific SPI intallation

--- src/resources/install.sh
+++ src/resources/install.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-GROUP_NAME=gpio
-USER=admin
-
-# create GPIO group
-addgroup $GROUP_NAME
-
-# assign group to user
-usermod -aG $GROUP_NAME $USER
-
-# copy udev rules
-cp 90-gpio.rules /etc/udev/rules.d
+. ./install_gpio.sh
+. ./install_spi.sh
 
