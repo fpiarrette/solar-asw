f1b8099bfa01c10e98db2ef2d4ed762d94cf09d8
Fernando Piarrette Caride <fm.piarrette@thalesaleniaspace.com>
2026-09-14 17:49:45 +0200

    Main organization and debug traces

--- src/main.c
+++ src/main.c
@@ -20,6 +20,8 @@
 
 #include <stdlib.h>
 
+static void configure_and_run(void);
+
 int main(int argc, char *argv[])
 {
     /* parse arguments */
@@ -37,96 +39,26 @@ int main(int argc, char *argv[])
 
         L_INFO("Starting...");
 
+#ifdef DEBUG
+        L_DEBUG("Debug version");
+#else
+        L_NOTICE("Release version");
+#endif
+
+#if PLATFORM_ID == PLATFORM_HOST
+        L_NOTICE("Platform: host");
+#else
+        L_NOTICE("Platform: target");
+#endif
+
         if (Config::getInstance()->isShowHelp())
         {
             Config::getInstance()->help(argc, argv);
         }
         else
         {
-            /* Platform implementation fixed at compilation time */
-            Platform::getInstance()->init();
-
-            Scheduller scheduller;
-
-            /* tasks */
-            TaskFromModem taskFromModem;
-            TaskToModem taskToModem;
-            TaskHumanInterface taskHumanInterface;
-            TaskIdle taskIdle;
-            TaskKiller taskKiller;
-            /* TaskRest taskRest; */
-
-            /* channels */
-            ChannelSocketClient channelSocketClient;
-            ChannelSocketServer channelSocketServer;
-            ChannelSocketServer channelSocketKillStop;
-            ChannelSpi channelSpi;
-            ChannelNull channelNull;
-
-            /* all channels are configured independently of the mode, because HOST mode could use a convination of them */
-            /* specific configuration for socket client */
-            channelSocketClient.setIpAddress(Config::getInstance()->getDestinationIpAddress());
-            channelSocketClient.setPort(Config::getInstance()->getDestinationPort());
-            /* specific configuration for socket server */
-            channelSocketServer.setPort(Config::getInstance()->getListeningPort());
-            /* specific configuration for SPI */
-            /* TBC */
-
-            if (Config::getInstance()->isFromModem())
-            {
-                /* specific task configuration */
-                taskFromModem.setTimeDeliveryLimit(Config::getInstance()->getTimeDeliveryLimit());
-                taskFromModem.setSizeLimit(Config::getInstance()->getBufferSizeLimit());
-
-                /* wiring */
-                taskFromModem.setSource(&channelSpi);
-                taskFromModem.setSink(&channelSocketClient);
-                scheduller.addTask(&taskFromModem, 2);
-            }
-            else
-            {
-                /* specific task configuration */
-
-                /* wiring */
-                taskToModem.setSource(&channelSocketServer);
-                taskToModem.setSink(&channelSpi);
-                scheduller.addTask(&taskToModem, 2);
-            }
-
-            /* set task killer a reception channel */
-            channelSocketKillStop.setPort(9090);
-            taskKiller.setSource(&channelSocketKillStop);
-            taskKiller.setScheduller(&scheduller);
-
-            /* manage REST interface */
-            /* scheduller.addTask(&taskRest, 16); */
-
-            /* manage human/GPIO interface */
-            scheduller.addTask(&taskHumanInterface, 20);
-
-            /* manage graceful kill stop flags */
-            scheduller.addTask(&taskKiller, 26);
-
-            scheduller.addTask(&taskIdle, 31);
-            taskIdle.setScheduller(&scheduller);
-
-#ifdef FORCE_TEST_CONTEXT
-            /* This compile time option allows to overwrite proper context and configure the process with a test context just for host platform and debug purpose */
-            if (Config::getInstance()->isFromModem())
-            {
-                /* specific task configuration */
-                taskFromModem.setSource(&channelSocketServer);
-            }
-            else
-            {
-                /* specific task configuration */
-                taskToModem.setSink(&channelSocketClient);
-            }
-
-#endif
-
-            /* execute all schedulled tasks */
-            scheduller.run();
+            /* perform all object required wiring and run the process */
+            configure_and_run();
         }
 
         /* free platform openned resources */
@@ -141,4 +73,94 @@ int main(int argc, char *argv[])
     }
 
     return EXIT_SUCCESS;
+}
+
+static void configure_and_run(void)
+{
+    /* Platform implementation fixed at compilation time */
+    Platform::getInstance()->init();
+
+    Scheduller scheduller;
+
+    /* tasks */
+    TaskFromModem taskFromModem;
+    TaskToModem taskToModem;
+    TaskHumanInterface taskHumanInterface;
+    TaskIdle taskIdle;
+    TaskKiller taskKiller;
+    /* TaskRest taskRest; */
+
+    /* channels */
+    ChannelSocketClient channelSocketClient;
+    ChannelSocketServer channelSocketServer;
+    ChannelSocketServer channelSocketKillStop;
+    ChannelSpi channelSpi;
+    ChannelNull channelNull;
+
+    /* all channels are configured independently of the mode, because HOST mode could use a convination of them */
+    /* specific configuration for socket client */
+    channelSocketClient.setIpAddress(Config::getInstance()->getDestinationIpAddress());
+    channelSocketClient.setPort(Config::getInstance()->getDestinationPort());
+    /* specific configuration for socket server */
+    channelSocketServer.setPort(Config::getInstance()->getListeningPort());
+    /* specific configuration for SPI */
+    /* TBC */
+
+    if (Config::getInstance()->isFromModem())
+    {
+        /* specific task configuration */
+        taskFromModem.setTimeDeliveryLimit(Config::getInstance()->getTimeDeliveryLimit());
+        taskFromModem.setSizeLimit(Config::getInstance()->getBufferSizeLimit());
+
+        /* wiring */
+        taskFromModem.setSource(&channelSpi);
+        taskFromModem.setSink(&channelSocketClient);
+        scheduller.addTask(&taskFromModem, 2);
+    }
+    else
+    {
+        /* specific task configuration */
+
+        /* wiring */
+        taskToModem.setSource(&channelSocketServer);
+        taskToModem.setSink(&channelSpi);
+        scheduller.addTask(&taskToModem, 2);
+    }
+
+    /* set task killer a reception channel */
+    channelSocketKillStop.setPort(9090);
+    taskKiller.setSource(&channelSocketKillStop);
+    taskKiller.setScheduller(&scheduller);
+
+    /* manage REST interface */
+    /* scheduller.addTask(&taskRest, 16); */
+
+    /* manage human/GPIO interface */
+    scheduller.addTask(&taskHumanInterface, 20);
+
+    /* manage graceful kill stop flags */
+    scheduller.addTask(&taskKiller, 26);
+
+    scheduller.addTask(&taskIdle, 31);
+    taskIdle.setScheduller(&scheduller);
+
+#if PLATFORM_ID == PLATFORM_HOST
+
+    L_WARNING("In host platform SPI channel is replaced by socket client and server channels");
+
+    /* This compile time option allows to overwrite proper context and configure the process with a test context just for host platform and debug purpose */
+    if (Config::getInstance()->isFromModem())
+    {
+        /* specific task configuration */
+        taskFromModem.setSource(&channelSocketServer);
+    }
+    else
+    {
+        /* specific task configuration */
+        taskToModem.setSink(&channelSocketClient);
+    }
+#endif
+
+    /* execute all schedulled tasks */
+    scheduller.run();
 }
