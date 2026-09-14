b2185a9a44562ecc97ac274567cc44b97eacc151
Fernando Piarrette <fernando.piarrette@gmail.com>
2026-09-14 16:08:57 +0200

    Utility functions verified

--- src/test_spi_ioctl.c
+++ src/test_spi_ioctl.c
@@ -1,3 +1,6 @@
+
+#include "test_common.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -40,7 +43,7 @@ int dump_status(int fd)
         return -1;
     }
 
-    printf("spi mode 0x%x, %d bits %sper word, %u Hz max", mode, bits, lsb ? "(lsb first) " : "", speed);
+    printf("spi mode 0x%x, %d bits %sper word, %u Hz max\n", mode, bits, lsb ? "(lsb first) " : "", speed);
 
     return 0;
 }
@@ -66,6 +69,28 @@ int main(int argc, char *argv[])
     }
 
     dump_status(fd);
+
+    char buffer1[256];
+    char buffer2[256];
+    int size;
+
+    if (str_2_hex(buffer1, argv[2], &size) < 0)
+    {
+        fprintf(stderr, "invalid argument %s\n", argv[2]);
+    }
+    else
+    {
+        if (hex_2_str(buffer2, buffer1, size) < 0)
+        {
+
+            fprintf(stderr, "invalid argument in hex to str\n");
+        }
+        else
+        {
+
+            fprintf(stdout, "reconverted %s\n", buffer2);
+        }
+    }
 
     if (close(fd) < 0)
     {
