855923160567559e5a6989a7fbfa2b1dc53d8235
Fernando Piarrette Caride <fm.piarrette@thalesaleniaspace.com>
2026-09-21 17:14:11 +0200

    Bug fixed building response

--- src/RestReplier.cpp
+++ src/RestReplier.cpp
@@ -11,7 +11,7 @@
 
 MHD_Result RestReplier::reply(struct MHD_Connection *connection, int code, const char *body, size_t size)
 {
-    MHD_Response *response = MHD_create_response_from_buffer(size, (void *)body, MHD_RESPMEM_PERSISTENT);
+    MHD_Response *response = MHD_create_response_from_buffer(size, (void *)body, MHD_RESPMEM_MUST_COPY);
     MHD_add_response_header(response, "Content-Type", "application/json");
     MHD_Result ret = MHD_queue_response(connection, code, response);
     MHD_destroy_response(response);
