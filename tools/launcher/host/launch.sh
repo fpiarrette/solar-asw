198cc93c64fe694cb20eeeb80b7678756a79a609
Fernando Piarrette Caride <fm.piarrette@thalesaleniaspace.com>
2026-09-24 18:05:17 +0200

    Iterative supported

--- tools/launcher/host/launch.sh
+++ tools/launcher/host/launch.sh
@@ -1,12 +1,37 @@
 #!/bin/sh
 
-# this script shall not be executed direclty, is intendeed to be called from main/parent script
+# reset getopts
+OPTIND=1
 
 # Docker flags
-FLAGS=-t
 WORKSPACE_DIR=/ws
+CMD="$WORKSPACE_DIR/$OUTPUT_DIR/$BINARY_NAME -l 1 -u"
+FLAGS="-t"
 
-CMD="$WORKSPACE_DIR/$OUTPUT_DIR/$BINARY_NAME"
+# this script shall not be executed direclty, is intendeed to be called from main/parent script
+show_help()
+{
+    echo "$0 [-i] [-h]"
+    echo "\t[-i] interactive"
+    echo "\t[-h] show this help"
+}
+
+while getopts "h?i" opt; do
+  case "$opt" in
+    h|\?)
+      show_help
+      exit 0
+      ;;
+    i)
+      FLAGS="${FLAGS} -i"
+      CMD="bash"
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+[ "${1:-}" = "--" ] && shift
 
 # launch make process
 podman run ${FLAGS} \
@@ -15,4 +40,4 @@ podman run ${FLAGS} \
   -p ${SOLAR_TARGET_REST_PORT}:${SOLAR_TARGET_REST_PORT} \
   -p ${SOLAR_TARGET_KILL_PORT}:${SOLAR_TARGET_KILL_PORT} \
   ${DOCKER_IMAGE_NAME} \
-  ${CMD} -l 1 -u
+  ${CMD}
